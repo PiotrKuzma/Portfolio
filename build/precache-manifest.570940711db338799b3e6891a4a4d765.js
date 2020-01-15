83d0ccf9a3824566d18a573b22182340164148b7
unknown <kuzma.boh@gmail.com>
2020-01-15 12:30:36 +0100

    production

---
+++ build/precache-manifest.570940711db338799b3e6891a4a4d765.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d516ffc877eb9c4847adf6292bd0749f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fcad1293bc70af9a9f9",
+    "url": "/static/css/main.20d2456d.chunk.css"
+  },
+  {
+    "revision": "f1c606295cdd080a37d6",
+    "url": "/static/js/2.1a97bb4a.chunk.js"
+  },
+  {
+    "revision": "1fcad1293bc70af9a9f9",
+    "url": "/static/js/main.8aadaee9.chunk.js"
+  },
+  {
+    "revision": "416cc1c79b8c22940a8c",
+    "url": "/static/js/runtime~main.7302273d.js"
+  },
+  {
+    "revision": "7f3ca0ce44f6e692c91ff1cc0e93f229",
+    "url": "/static/media/avatar.7f3ca0ce.jpg"
+  },
+  {
+    "revision": "3b070670c39480c4d18a34f2400fc4d4",
+    "url": "/static/media/chart.3b070670.svg"
+  },
+  {
+    "revision": "267ba708d73d2b59a1d94bf1ba20f275",
+    "url": "/static/media/code.267ba708.svg"
+  },
+  {
+    "revision": "9f84ccf7237aa5ff559a945469fdc71c",
+    "url": "/static/media/code.9f84ccf7.svg"
+  },
+  {
+    "revision": "3298443f3d60529586f10f7fd9c9b6de",
+    "url": "/static/media/display.3298443f.svg"
+  },
+  {
+    "revision": "72432601f80b8aaed3e33b7fb5a90c66",
+    "url": "/static/media/git.72432601.svg"
+  },
+  {
+    "revision": "1fc7ff0b941570453667b57847926e4c",
+    "url": "/static/media/github.1fc7ff0b.svg"
+  },
+  {
+    "revision": "38ff3f4cbb2f8f3f80f902e2cb48cac4",
+    "url": "/static/media/html5.38ff3f4c.svg"
+  },
+  {
+    "revision": "4a4cf26c30e6ff5211b0cac89b2d3e79",
+    "url": "/static/media/js.4a4cf26c.svg"
+  },
+  {
+    "revision": "f80a0f2404d47756f6cece2974ac4b4c",
+    "url": "/static/media/linkedin.f80a0f24.svg"
+  },
+  {
+    "revision": "8c29c6dff5d02f138383f3de48edfae5",
+    "url": "/static/media/node.8c29c6df.svg"
+  },
+  {
+    "revision": "31f1418292ad181242285fcc5d13b178",
+    "url": "/static/media/react.31f14182.svg"
+  },
+  {
+    "revision": "777015f64c284057939cd39b4e60cc26",
+    "url": "/static/media/sass.777015f6.svg"
+  },
+  {
+    "revision": "3040e8f7a0c80e1201d45c586dc8138e",
+    "url": "/static/media/team.3040e8f7.svg"
+  }
+]);
